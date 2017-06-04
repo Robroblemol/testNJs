bedad29f203d37be071cdbb05939609168a52b7f
Raziel <robertodavidrobles@gmail.com>
2017-06-04 14:16:53 -0500

    Test action blocking

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,12 @@
 function start() {
   console.log("manipulador de peticion 'start' se ha llamdado");
+    function sleep(milliSeconds) {
+      //obtenemos hora actual
+      var startTime = new Date().getTime();
+      //ocupamos la cpu inutilmente
+      while (new Date().getTime()<startTime+milliSeconds);
+    }
+  sleep(10000);
   return "Hola inicio";//repornar directamente desde el nanejador
 }
 function up() {
