1f3fe6a4d23e67572ca8b527dc3af2e300593ebc
Raziel <robertodavidrobles@gmail.com>
2017-06-11 14:11:47 -0500

    add form in handl init, just for test

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,23 @@
 var exec = require("child_process").exec;
 function start(response) {
   console.log("manipulador de peticion 'start' se ha llamdado");
-  exec("ls -lah",function (error,stdout,stderr) {
+  var body = '<html>'+
+  '<head>'+
+
+    '</head>'+
+    '<body>'+
+    '<form action="/up" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Enviar texto" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+
     response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(stdout);
+    response.write(body);
     response.end();
-  });
+
   //return "Hola inicio";//repornar directamente desde el nanejador
 }
 function up(response) {
